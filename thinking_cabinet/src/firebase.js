a605242865a418d6915abc796fb37d8f5677fc97
DupieM <221326@virtualwindow.co.za>
2025-11-16 13:10:18 +0200

    update

--- thinking_cabinet/src/firebase.js
+++ thinking_cabinet/src/firebase.js
@@ -8,7 +8,7 @@ import { getStorage } from 'firebase/storage';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB_Fg_vwk2fxoHLqO8llI4eV5cpmTPjVjc",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "the-thinking-cabinet.firebaseapp.com",
   projectId: "the-thinking-cabinet",
   storageBucket: "the-thinking-cabinet.firebasestorage.app",
